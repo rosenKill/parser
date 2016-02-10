958feee00adba5c76503e41cc42d655c63828fc6
Oleg <GrinjukOleg@gmail.com>
2016-02-10 12:54:43 +0300

    fix schedule with different subgroup in one day and different week

--- x.py
+++ x.py
@@ -16,6 +16,7 @@ def returnScsIdtf(idGroup, nameSubjectAndType):
 def writeConturs():
     # nameSCSI="321701_"+day+id;
     # f=open(nameSCSI,"w")
+    createNewFile=0   #для подгрупп если обе подгруппы в одно время но по разным неделям,как у нас в четверг пбз ЛБ
     nameSubject = predmet.find("subject").text
     if (nameSubject != "СпецПодг" and nameSubject != "ФизК"):
         teacher = predmet.find("employee")
@@ -27,7 +28,7 @@ def writeConturs():
         time = predmet.find("lessonTime").text
         numberPair = numbersPairs[time]
         nameContur = "spring2016" + "G" + str(
-            shortNameGroupInt) + nameSubject + "_" + typeSubject + nameDay + numberPair
+            shortNameGroupInt) + engShortNamesSubject[nameSubject] + "_" + engNameTypeSubject[typeSubject] + engDay[nameDay] + numberPair
         nameFile = path + nameContur
         # print(nameFile)
         nameFileScs = nameFile + ".scs"
@@ -36,11 +37,13 @@ def writeConturs():
         smallNameFileScsI = path + "small" + nameContur + ".scsi"
         subGroup=predmet.find("numSubgroup").text
         idtfGroup=""
-        if(subGroup==0):
+        if(subGroup=="0"):
             idtfGroup=currentGroup
-        elif(subGroup==1):
+        elif(subGroup=="1"):
+            createNewFile=1
             idtfGroup=currentGroup+"_"+"1"
         else:
+            createNewFile=1
             idtfGroup=currentGroup+"_"+"2"
         if not (os.path.exists(nameFileScs)):  # else мы дописываем только в мелкий контур а scs и большй контур не трогаем
             weeks = predmet.findall("weekNumber")
@@ -56,7 +59,7 @@ def writeConturs():
                 fSCSI.write("->...(*\n")
                 fSCSI.write("<-time_point;;\n")
                 fSCSI.write("->rrel_number_of_pair:" + numberPair + " ;;\n")
-                fSCSI.write("->rrel_day:" + nameDay + ";;\n")
+                fSCSI.write("->rrel_day:" + engDay[nameDay] + ";;\n")
                 fSCSI.write("->rrel_week:" + week.text + " ;;\n")
                 fSCSI.write(";;*);;\n")
 
@@ -70,37 +73,105 @@ def writeConturs():
             rightNameSubject=returnScsIdtf(str(shortNameGroupInt),nameSubjectAndType)
             print(rightNameSubject)
             smallFSCSI.write(rightNameSubject +"=>nrel_teach:"+teachers[teacherId]+"; \n")
-            smallFSCSI.write("<=group"+idtfGroup+";;")
+            smallFSCSI.write("<=group"+idtfGroup+";;\n\n")
             smallFSCSI.close()
 
         else:
-            print("dopisivaem in small.scsi")
-            smallFSCSI = open(smallNameFileScsI, "a")
-            nameSubjectAndType = nameSubject + "_" + typeSubject
-            print(nameSubjectAndType)
-            rightNameSubject=returnScsIdtf(str(shortNameGroupInt),nameSubjectAndType)
-            print(rightNameSubject)
-            smallFSCSI.write(rightNameSubject +"=>nrel_teach:"+teachers[teacherId]+"; \n")
-            smallFSCSI.write("<=group"+idtfGroup+";;")
-            smallFSCSI.close()
+            if(createNewFile==0):
+                print("dopisivaem in small.scsi")
+                smallFSCSI = open(smallNameFileScsI, "a")
+                nameSubjectAndType = nameSubject + "_" + typeSubject
+                print(nameSubjectAndType)
+                rightNameSubject=returnScsIdtf(str(shortNameGroupInt),nameSubjectAndType)
+                print(rightNameSubject)
+                smallFSCSI.write(rightNameSubject +"=>nrel_teach:"+teachers[teacherId]+"; \n")
+                smallFSCSI.write("<=group"+idtfGroup+";;\n\n")
+                smallFSCSI.close()
+            elif(createNewFile==1):
+                smallNameContur = "small" + nameContur+idtfGroup
+                smallNameFileScsI = path + smallNameContur + ".scsi"
+                nameFileScsI = nameFile + idtfGroup+".scsi"
+                weeks = predmet.findall("weekNumber")
+                nameFileScs=nameFile+idtfGroup+".scs"
+                fSCS = open(nameFileScs, "w")
+                fSCS.write(nameContur + idtfGroup + ' = [*^" ' + nameFileScsI + '"*];;\n')
+                fSCS.write("spring2016->" + nameContur + idtfGroup + ";;")
+                fSCS.close()
+
+                fSCSI = open(nameFileScsI, "w")
+                fSCSI.write(smallNameContur + ' = [*^" file://' + smallNameFileScsI + ' "*];; \n')
+                fSCSI.write(smallNameContur + " => nrel_time_situation: ...\n (*")
+                for week in weeks:
+                    fSCSI.write("->...(*\n")
+                    fSCSI.write("<-time_point;;\n")
+                    fSCSI.write("->rrel_number_of_pair:" + numberPair + " ;;\n")
+                    fSCSI.write("->rrel_day:" + engDay[nameDay] + ";;\n")
+                    fSCSI.write("->rrel_week:" + week.text + " ;;\n")
+                    fSCSI.write(";;*);;\n")
+
+                fSCSI.write("*);;")
+                fSCSI.close()
+                #             for small contur
+
+                smallFSCSI = open(smallNameFileScsI, "w")
+                nameSubjectAndType = nameSubject + "_" + typeSubject
+                print(nameSubjectAndType)
+                rightNameSubject=returnScsIdtf(str(shortNameGroupInt),nameSubjectAndType)
+                print(rightNameSubject)
+                smallFSCSI.write(rightNameSubject +"=>nrel_teach:"+teachers[teacherId]+"; \n")
+                smallFSCSI.write("<=group"+idtfGroup+";;\n\n")
+                smallFSCSI.close()
+            else:
+                print("c---------------------------------------------")
 
 
 
 
 # xml_distant = urllib.request.urlopen('xml.xml')   #http://www.bsuir.by/schedule/rest/schedule/21010
 groupes = ("321701", "321702", "321703")
-teachers = {"504529": "parkalov",
-            "502183": "shunja",
-            "504551": "rusetckii",
-            "500393": "stepanova",
-            "500382": "ivash",
-            "500384": "kolb",
-            "500385": "koronchik",
-            "500378": "it dav"
+# groupes = ("421701", "421702", "421703")
+teachers = {"504529": "Parkalov_Alexey_Viktorovich",
+            "502183": "shunkevich_d_v",
+            "504551": "rusetski_k_v",
+            "500393": "Stepanova_Margarita_Dmitrievna",
+            "500382": "Ivashenko_Valerian_Petrovich",
+            "500384": "Kolb_Dmitry_Grigorxevich",
+            "500385": "Koronchik_Denis_Nikolaevich",
+            "500378": "Davydenko_Irina_Timofeevna ",
+            "502185": "romanov ",
+            "500381": "zaharov ",
+            "504356": "shatilo nikolai ",
+            "505991": "korotkevich dmitrii alersandrovich ",
+            "500368": " pavlovec juri",
+            "500511": "belskii aleksei ",
+            "504528": "zhukov II ",
+            "500377": "gulyakina ",
+            "504367": "adamovich_vadim ",
+            "504358": "Stoler denis "
             }
-groupesCode = {"321701": "21010", }
-numbersPairs = {"08:00-09:35": "FIRST", "09:45-11:20": "SECOND", "11:40-13:15": "THIRD", "13:25-15:00": "FOURTH",
-                "15:20-16:55": "FIFTH", "17:05-18:40": "SIXTH"}
+engShortNamesSubject = {"АиПОСиЗИ" : "aipos" ,
+                     "ЯПИС" : "japis" ,
+                     "ЛОИС" : "lois" ,
+                     "ОИз" : "oiz" ,
+                     "ПБЗ" : "pbz" ,
+                     "ЕЯзИИС" : "ejaiis" ,
+                     "СтатОИВ" : "StatOiv"
+                     }
+engNameTypeSubject = {"ЛР" : "" ,
+                      "ПЗ" : "" ,
+                      "ЛК" : "" ,
+                      }
+engDay = { "Понедельник" : "Monday" ,
+           "Вторник" : "Tuesday" ,
+           "Среда" : "Wednesday" ,
+           "Четверг" : "Thursday" ,
+           "Пятница" : "Friday" ,
+           "Суббота" : "Saturday" ,
+           }
+groupesCode = {"321701": "21010", "321702": "21079", "321703": "21080" }
+# groupesCode = {"421701": "21139", "421702": "21207", "421703": "21208" }
+numbersPairs = {"08:00-09:35": "first_pair", "09:45-11:20": "second_pair", "11:40-13:15": "third_pair", "13:25-15:00": "fourth_pair",
+                "15:20-16:55": "fifth_pair", "17:05-18:40": "sixth_pair", "18:45-20:20": "seventh_pair", "20:25-22:00": "eigthth_pair"}
 dict321 = {"АиПОСиЗИ_ЛР": "laboratory_work_on_apoczi_4_year_education",
            "ЯПИС_ЛР": "laboratory_work_on_language_processors_of_intelligent_systems",
            "ЛОИС_ЛР": "laboratory_work_on_lois_4_year_1",
@@ -119,18 +190,22 @@ dict321 = {"АиПОСиЗИ_ЛР": "laboratory_work_on_apoczi_4_year_education"
            "ЯПИС_ЛК": "lectures_on_language_processors_of_intelligent_systems",
            "ЛОИС_ЛК": "lectures_on_lois_4_year_1",
            "ОИз_ЛК": "lectures_on_analysis_of_image_processing" }
-
+# 421701 -21139
+# 421702 - 21207
+# 421703- 21208
 # cycle for all groupes
-currentGroup=groupes[0]
-row = groupesCode["321701"] + ".xml"
-print(row)
-tree = etree.parse(row)
-root = tree.getroot()
-allDays = root.findall('scheduleModel')
-for day in allDays:  # day == scheduleModel
-    nameDay = day.find("weekDay").text
-    print("--------" + nameDay)
-    predmetsInDay = day.findall("schedule")  # all predmets in day . f.e. in wednesday predmetsInDay=1 (specPodgotovka)
-    for predmet in predmetsInDay:
-        # print(predmet.find("subject").text)
-        writeConturs();
+# var=0
+for var in groupes:
+    currentGroup=var
+    row = groupesCode[var] + ".xml"
+    print(row)
+    tree = etree.parse(row)
+    root = tree.getroot()
+    allDays = root.findall('scheduleModel')
+    for day in allDays:  # day == scheduleModel
+        nameDay = day.find("weekDay").text
+        print("--------" + nameDay)
+        predmetsInDay = day.findall("schedule")  # all predmets in day . f.e. in wednesday predmetsInDay=1 (specPodgotovka)
+        for predmet in predmetsInDay:
+            # print(predmet.find("subject").text)
+            writeConturs();
